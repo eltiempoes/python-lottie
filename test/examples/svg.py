4b530702255f70c270c5886acc9e40d78a1f37df
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-10 10:19:31 +0200

    SVG demo file

---
+++ test/examples/svg.py
@@ -0,0 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+    "lib"
+))
+from tgs import exporters
+from tgs import objects
+
+
+an = parse_svg_file(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "blep.svg"
+))
+
+layer = an.find("durg")
+layer.transform.anchor_point.value = [256, 256]
+layer.transform.position.value = [256, 256]
+layer.transform.rotation.add_keyframe(0, 0)
+layer.transform.rotation.add_keyframe(30, 180)
+layer.transform.rotation.add_keyframe(60, 360)
+
+
+exporters.multiexport(an, "/tmp/svg")
