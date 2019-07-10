8ac22eb6ab5bb19fa27a40c9cf57711c600b4cae
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-10 16:44:14 +0200

    Option for a more concise lottiecat

--- bin/lottiecat.py
+++ bin/lottiecat.py
@@ -3,6 +3,7 @@
 import json
 import sys
 import os
+import argparse
 sys.path.append(os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     "lib"
@@ -10,6 +11,26 @@ sys.path.append(os.path.join(
 from tgs.exporters import prettyprint, prettyprint_summary
 from tgs.objects import Animation
 
+parser = argparse.ArgumentParser(
+    description="Pretty prints a lottie file"
+)
+parser.add_argument(
+    "infile",
+    help="Input file"
+)
+parser.add_argument(
+    "--summary",
+    "-s",
+    action="store_true",
+    help="Just show a short summary"
+)
 
-prettyprint_summary(Animation.load(json.load(open(sys.argv[1]))))
+ns = parser.parse_args()
+
+an = Animation.load(json.load(open(ns.infile)))
+
+if ns.summary:
+    prettyprint_summary(an)
+else:
+    prettyprint(an)
 
