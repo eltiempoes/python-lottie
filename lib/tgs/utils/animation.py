a8b768cb7764064843aaee0e7cd0dac7b1acc947
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-10 19:17:36 +0200

    Rotational shake

--- lib/tgs/utils/animation.py
+++ lib/tgs/utils/animation.py
@@ -1,14 +1,30 @@
 import random
 
 
-def shake(transformable, x_radius, y_radius, start_time, end_time, n_frames):
+def shake(position_prop, x_radius, y_radius, start_time, end_time, n_frames):
     frame_time = (end_time - start_time) / n_frames
-    if not transformable.position.animated:
-        start_x, start_y = transformable.position.value
+    if not position_prop.animated:
+        start_x, start_y = position_prop.value
     else:
-        start_x, start_y = transformable.position.keyframes[-1].start
+        start_x, start_y = position_prop.keyframes[-1].start
 
     for i in range(n_frames):
         x = start_x + (random.random() * 2 - 1) * x_radius
         y = start_y + (random.random() * 2 - 1) * y_radius
-        transformable.position.add_keyframe(start_time + i * frame_time, [x, y])
+        position_prop.add_keyframe(start_time + i * frame_time, [x, y])
+
+
+def rot_shake(rotation_prop, min_angle, max_angle, start_time, end_time, n_frames):
+    frame_time = (end_time - start_time) / n_frames
+    if not rotation_prop.animated:
+        start = rotation_prop.value
+    else:
+        start = rotation_prop.keyframes[-1].start
+    delta = max_angle - min_angle
+
+    first = 0
+    rotation_prop.add_keyframe(start_time, start)
+    for i in range(1, n_frames):
+        a = start + min_angle + (random.random() * delta)
+        rotation_prop.add_keyframe(start_time + i * frame_time, a)
+    rotation_prop.add_keyframe(end_time, start)
--- test/examples/shake.py
+++ test/examples/shake.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(
 ))
 from tgs import exporters
 from tgs import objects
-from tgs.utils.animation import shake
+from tgs.utils.animation import shake, rot_shake
 
 an = objects.Animation(59)
 
@@ -15,9 +15,16 @@ an.add_layer(layer)
 
 circle = layer.add_shape(objects.Ellipse())
 circle.size.value = [100, 100]
-circle.position.value = [256, 256]
+circle.position.value = [256, 128]
 
-shake(circle, 10, 15, 0, 59, 25)
+shake(circle.position, 10, 15, 0, 59, 25)
+
+
+g = layer.add_shape(objects.Group())
+box = g.add_shape(objects.Rect())
+box.size.value = [200, 100]
+g.transform.anchor_point.value = g.transform.position.value = box.position.value = [256, 384]
+rot_shake(g.transform.rotation, -15, 15, 0, 60, 10)
 
 
 layer.add_shape(objects.Fill([1, 1, 0]))
