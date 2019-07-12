8e9042c3684f68780afbbabe6afa37f7a68c1d9b
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-12 12:53:49 +0200

    Allow multiple properties on shake

--- lib/tgs/utils/animation.py
+++ lib/tgs/utils/animation.py
@@ -3,56 +3,31 @@ import math
 
 
 def shake(position_prop, x_radius, y_radius, start_time, end_time, n_frames):
+    if not isinstance(position_prop, list):
+        position_prop = [position_prop]
+
+    n_frames = int(round(n_frames))
     frame_time = (end_time - start_time) / n_frames
-    if not position_prop.animated:
-        start_x, start_y = position_prop.value
-    else:
-        start_x, start_y = position_prop.keyframes[-1].start
+    startpoints = list(map(
+        lambda pp: pp.get_value(start_time),
+        position_prop
+    ))
 
     for i in range(n_frames):
-        x = start_x + (random.random() * 2 - 1) * x_radius
-        y = start_y + (random.random() * 2 - 1) * y_radius
-        position_prop.add_keyframe(start_time + i * frame_time, [x, y])
+        x = (random.random() * 2 - 1) * x_radius
+        y = (random.random() * 2 - 1) * y_radius
+        for pp, start in zip(position_prop, startpoints):
+            px = start[0] + x
+            py = start[1] + y
+            pp.add_keyframe(start_time + i * frame_time, [px, py])
 
-
-def random_rot_shake(rotation_prop, min_angle, max_angle, start_time, end_time, n_frames):
-    frame_time = (end_time - start_time) / n_frames
-    if not rotation_prop.animated:
-        start = rotation_prop.value
-    else:
-        start = rotation_prop.keyframes[-1].start
-    delta = max_angle - min_angle
-
-    first = 0
-    rotation_prop.add_keyframe(start_time, start)
-    for i in range(1, n_frames):
-        a = start + min_angle + (random.random() * delta)
-        rotation_prop.add_keyframe(start_time + i * frame_time, a)
-    rotation_prop.add_keyframe(end_time, start)
-
-
-def random_rot_shake(rotation_prop, min_angle, max_angle, start_time, end_time, n_frames):
-    frame_time = (end_time - start_time) / n_frames
-    if not rotation_prop.animated:
-        start = rotation_prop.value
-    else:
-        start = rotation_prop.keyframes[-1].start
-    delta = max_angle - min_angle
-
-    first = 0
-    rotation_prop.add_keyframe(start_time, start)
-    for i in range(1, n_frames):
-        a = start + min_angle + (random.random() * delta)
-        rotation_prop.add_keyframe(start_time + i * frame_time, a)
-    rotation_prop.add_keyframe(end_time, start)
+    for pp, start in zip(position_prop, startpoints):
+        pp.add_keyframe(end_time, start)
 
 
 def rot_shake(rotation_prop, angles, start_time, end_time, n_frames):
     frame_time = (end_time - start_time) / n_frames
-    if not rotation_prop.animated:
-        start = rotation_prop.value
-    else:
-        start = rotation_prop.keyframes[-1].start
+    start = rotation_prop.get_value(start_time)
 
     first = 0
     for i in range(0, n_frames):
