4de3001f619015575b3ba329b89ebb69a7f4dcce
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-12 16:40:30 +0200

    Color conversion script

---
+++ bin/color_convert.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "lib"
+))
+from tgs.parsers.svg.importer import parse_color
+
+print(parse_color(sys.argv[1]))
+
