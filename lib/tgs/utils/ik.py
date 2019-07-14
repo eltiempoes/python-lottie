6866af52cfc9e6e80c2eb1bb1cc9ec118314a01c
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-14 22:44:45 +0200

    Rename segments to joints

--- lib/tgs/utils/ik.py
+++ lib/tgs/utils/ik.py
@@ -4,58 +4,58 @@ from .nvector import NVector
 # FABRIK
 class Chain:
     def __init__(self, tail, fixed_tail=True, tolerance=0.5, max_iter=8):
-        self.segments = [tail.clone()]
+        self.joints = [tail.clone()]
         self.fixed_tail = fixed_tail
         self.lengths = []
         self.total_length = 0
         self.tolerance = tolerance
         self.max_iter = max_iter
 
-    def add_point(self, point):
-        length = (point - self.segments[-1]).length
+    def add_joint(self, point):
+        length = (point - self.joints[-1]).length
         self.lengths.append(length)
         self.total_length += length
-        self.segments.append(point.clone())
+        self.joints.append(point.clone())
 
     def backward(self, target):
         """
         target -> -> start
         """
-        self.segments[-1] = target
-        for i in range(len(self.segments)-2, -1, -1):
-            r = self.segments[i+1] - self.segments[i]
+        self.joints[-1] = target
+        for i in range(len(self.joints)-2, -1, -1):
+            r = self.joints[i+1] - self.joints[i]
             l = self.lengths[i] / r.length
-            self.segments[i] = self.segments[i+1].lerp(self.segments[i], l)
+            self.joints[i] = self.joints[i+1].lerp(self.joints[i], l)
 
     def forward(self, target):
         """
         start -> -> tail
         """
-        self.segments[0] = target
-        for i in range(0, len(self.segments)-1):
-            r = self.segments[i+1] - self.segments[i]
+        self.joints[0] = target
+        for i in range(0, len(self.joints)-1):
+            r = self.joints[i+1] - self.joints[i]
             l = self.lengths[i] / r.length
-            self.segments[i+1] = self.segments[i].lerp(self.segments[i+1], l)
+            self.joints[i+1] = self.joints[i].lerp(self.joints[i+1], l)
 
     def reach(self, target):
         if not self.fixed_tail:
             self.backward(target)
             return
 
-        distance = (target - self.segments[0]).length
+        distance = (target - self.joints[0]).length
         if distance >= self.total_length:
-            for i in range(len(self.segments)-1):
-                r = target - self.segments[i]
+            for i in range(len(self.joints)-1):
+                r = target - self.joints[i]
                 l = self.lengths[i] / r.length
-                self.segments[i+1] = self.segments[i].lerp(target, l)
+                self.joints[i+1] = self.joints[i].lerp(target, l)
             return
 
-        base = self.segments[0]
+        base = self.joints[0]
 
-        distance = (target - self.segments[-1]).length
+        distance = (target - self.joints[-1]).length
         n_it = 0
         while distance > self.tolerance and n_it < self.max_iter:
             self.backward(target)
             self.forward(base)
-            distance = (target - self.segments[-1]).length
+            distance = (target - self.joints[-1]).length
             n_it += 1
