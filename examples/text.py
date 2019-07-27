ae362e076bac733a1efe5ec37459e2151b5dd602
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-07-27 09:33:34 +0200

    Add text example

---
+++ examples/text.py
@@ -0,0 +1,26 @@
+"""
+@note Text layers are not supported by telegram
+"""
+import sys
+import os
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "lib"
+))
+from tgs import exporters
+from tgs import objects
+from tgs import Color, Point
+
+
+
+an = objects.Animation(120)
+an.fonts = objects.text.FontList()
+an.fonts.append(objects.text.Font("sans", name="sans"))
+layer = objects.TextLayer()
+an.add_layer(layer)
+layer.data.add_keyframe(0, objects.text.TextItem("Text", 200, Color(1, 0, 0), "sans"))
+layer.data.add_keyframe(60, objects.text.TextItem("Here", 200, Color(0, 1, 0), "sans"))
+layer.transform.position.value = Point(30, 200)
+
+
+exporters.multiexport(an, "/tmp/text")
