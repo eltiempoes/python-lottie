ce73d58add793b54f023bbe9a665d4806a01d732
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-08-11 21:01:32 +0200

    Allow passing file objects to parse_tgs_json

--- lib/tgs/parsers/tgs.py
+++ lib/tgs/parsers/tgs.py
@@ -1,20 +1,32 @@
+import io
 import json
 import gzip
 from ..objects import Animation
 
 
-def parse_tgs_json(filename):
+def parse_tgs_json(file):
     """!
     Reads both tgs and lottie files, returns the json structure
     """
-    with open(filename, "r") as file:
-        mn = file.buffer.read(2)
-        file.buffer.seek(0)
-        if mn == b'\x1f\x8b': # gzip magic number
-            json_file = gzip.open(file.buffer, "rb")
-        else:
-            json_file = file
-        return json.load(json_file)
+    if isinstance(file, str):
+        with open(file, "r") as fileobj:
+            return parse_tgs_json(fileobj)
+
+    if isinstance(file, io.TextIOBase):
+        binfile = file.buffer
+    else:
+        binfile = file
+
+    mn = binfile.read(2)
+    binfile.seek(0)
+    if mn == b'\x1f\x8b': # gzip magic number
+        json_file = gzip.open(binfile, "rb")
+    elif isinstance(file, io.TextIOBase):
+        json_file = file
+    else:
+        json_file = io.TextIOWrapper(file)
+
+    return json.load(json_file)
 
 
 def parse_tgs(filename):
