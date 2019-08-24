988464ef078433c02c71643bbb2e9141527397fe
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-08-24 07:48:37 +0200

    Improve the color print script

--- bin/tgsprintcolor.py
+++ bin/tgsprintcolor.py
@@ -2,12 +2,25 @@
 
 import sys
 import os
+import re
 import argparse
 sys.path.append(os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     "lib"
 ))
 from tgs.parsers.svg.importer import parse_color
+from tgs import NVector
+
+
+def _parse_color(color):
+    # Inkscape colors
+    if re.match(r"^[0-9a-fA-F]{8}$", color):
+        return NVector(*(
+            int(color[i:i+2], 16) / 0xff
+            for i in range(0, 8, 2)
+        ))
+    return parse_color(color)
+
 
 
 parser = argparse.ArgumentParser(
@@ -20,5 +33,5 @@ parser.add_argument(
 
 if __name__ == "__main__":
     ns = parser.parse_args()
-    print(parse_color(ns.color).components)
+    print(_parse_color(ns.color).components)
 
