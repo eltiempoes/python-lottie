24bf6f4fcd885acf15eed4df15d26f8f38761802
Mattia Basaglia <mattia.basaglia@gmail.com>
2019-08-27 23:20:15 +0200

    HCL

--- examples/colorize.py
+++ examples/colorize.py
@@ -19,12 +19,14 @@ an = parse_svg_file(os.path.join(
 layer = an.find("durg")
 
 n_frames = 24
-
 for fill in layer.find_all((objects.Fill, objects.Stroke)):
-    color = ManagedColor.from_color(fill.color.value).convert(ManagedColor.Mode.HSV)
+    color = ManagedColor.from_color(fill.color.value)
+    color.convert(ManagedColor.Mode.HSV)
+
     for frame in range(n_frames):
         off = frame / (n_frames-1)
         color.hue = (color.hue + 1 / (n_frames-1)) % 1
         fill.color.add_keyframe(off * last_frame, color.converted(ManagedColor.Mode.RGB).vector)
+
 
 script.script_main(an)
--- lib/tgs/utils/color.py
+++ lib/tgs/utils/color.py
@@ -1,4 +1,5 @@
 import enum
+import math
 import colorsys
 from ..nvector import NVector, Color
 
@@ -11,6 +12,7 @@ class ColorMode(enum.Enum):
     RGB = enum.auto()
     HSV = enum.auto()
     HSL = enum.auto()
+    HCL = enum.auto()
 
 
 def _rgb_to_hsl(r, g, b):
@@ -18,8 +20,73 @@ def _rgb_to_hsl(r, g, b):
     return (h, s, l)
 
 
+# http://w3.uqo.ca/missaoui/Publications/TRColorSpace.zip
+def rgb_to_hcl(r, g, b, gamma=3, y0=100):
+    maxc = max(r, g, b)
+    minc = min(r, g, b)
+    if maxc > 0:
+        alpha = 1/y0 * minc / maxc
+    else:
+        alpha = 0
+    q = math.e ** (alpha * gamma)
+    h = math.atan2(g - b, r - g)
+    if h < 0:
+        h += 2*math.pi
+    h /= 2*math.pi
+    c = q / 3 * (abs(r-g) + abs(g-b) + abs(b-r))
+    l = (q * maxc + (q-1) * minc) / 2
+    return (h, c, l)
+
+
+def _clamp(x):
+    return max(0, min(1, x))
+
+
+def hcl_to_rgb(h, c, l, gamma=3, y0=100):
+    h *= 2*math.pi
+
+    q = math.e ** ((1 - 2*c / 4*l) * gamma / y0)
+    minc = (4*l - 3*c) / (4*q - 2)
+    maxc = minc + 3*c / 2*q
+
+    if h <= math.pi * 1 / 3:
+        tan = math.tan(3/2*h)
+        r = maxc
+        b = minc
+        g = (r * tan + b) / (1 + tan)
+    elif h <= math.pi * 2 / 3:
+        tan = math.tan(3/4*(h-math.pi))
+        g = maxc
+        b = minc
+        r = (g * (1+tan) - b) / tan
+    elif h <= math.pi * 3 / 3:
+        tan = math.tan(3/4*(h-math.pi))
+        g = maxc
+        r = minc
+        b = g * (1+tan) - r * tan
+    elif h <= math.pi * 4 / 3:
+        tan = math.tan(3/2*(h+math.pi))
+        b = maxc
+        r = minc
+        g = (r * tan + b) / (1 + tan)
+    elif h <= math.pi * 5 / 3:
+        tan = math.tan(3/4*h)
+        b = maxc
+        g = minc
+        r = (g * (1+tan) - b) / tan
+    else:
+        tan = math.tan(3/4*h)
+        r = maxc
+        g = minc
+        b = g * (1+tan) - r * tan
+
+    return _clamp(r), _clamp(g), _clamp(b)
+
+
 colorsys.hsl_to_rgb = lambda h, s, l: colorsys.hls_to_rgb(h, l, s)
 colorsys.rgb_to_hsl = _rgb_to_hsl
+colorsys.rgb_to_hcl = rgb_to_hcl
+colorsys.hcl_to_rgb = hcl_to_rgb
 
 
 class ManagedColor:
@@ -33,10 +100,7 @@ class ManagedColor:
     def mode(self):
         return self._mode
 
-    def convert(self, v):
-        if v == self._mode:
-            return self
-
+    def _convert(self, v):
         if self._mode == ColorMode.RGB or v == ColorMode.RGB:
             conv = getattr(colorsys, "%s_to_%s" % (self._mode.name.lower(), v.name.lower()))
             self.vector = NVector(*conv(*self.vector))
@@ -57,6 +121,12 @@ class ManagedColor:
         else:
             raise ValueError
 
+    def convert(self, v):
+        if v == self._mode:
+            return self
+
+        self._convert(v)
+
         self._mode = v
         return self
 
@@ -74,30 +144,27 @@ class ManagedColor:
         return cls(*color[:3], ColorMode.RGB)
 
     def __repr__(self):
-        return "<%s %s %s>" % (self.__class__.__name__, self.mode.name, self.vector.components)
+        return "<%s %s [%.3f, %.3f, %.3f]>" % (
+            (self.__class__.__name__, self.mode.name) + tuple(self.vector.components)
+        )
 
     def _attrindex(self, name):
+        comps = None
+
         if self._mode == ColorMode.RGB:
-            if name in {"r", "red"}:
-                return 0
-            if name in {"g", "green"}:
-                return 1
-            if name in {"b", "blue"}:
-                return 2
+            comps = ({"r", "red"}, {"g", "green"}, {"b", "blue"})
         elif self._mode == ColorMode.HSV:
-            if name in {"h", "hue"}:
-                return 0
-            if name in {"s", "saturation"}:
-                return 1
-            if name in {"v", "value"}:
-                return 2
+            comps = ({"h", "hue"}, {"s", "saturation"}, {"v", "value"})
         elif self._mode == ColorMode.HSL:
-            if name in {"h", "hue"}:
-                return 0
-            if name in {"s", "saturation"}:
-                return 1
-            if name in {"l", "lightness"}:
-                return 2
+            comps = ({"h", "hue"}, {"s", "saturation"}, {"l", "lightness"})
+        elif self._mode == ColorMode.HCL:
+            comps = ({"h", "hue"}, {"c", "choma"}, {"l", "luma", "luminance"})
+
+        if comps:
+            for i, vals in enumerate(comps):
+                if name in vals:
+                    return i
+
         return None
 
     def __getattr__(self, name):
