9d4579da6a69f284cf8c3e0fa76aec18274e311b
Mattia Basaglia <glax@dragon.best>
2020-04-01 16:10:20 +0200

    New synfig import/export interface

--- addons/synfig/tgs-exporter/tgs-exporter.py
+++ addons/synfig/tgs-exporter/tgs-exporter.py
@@ -14,17 +14,20 @@ parser.add_argument("outfile", nargs="?")
 
 ns = parser.parse_args()
 
-with open(ns.infile) as sif_file:
-    animation = tgs.parsers.sif.parse_sif_file(sif_file)
+importer = tgs.importers.importers.get_from_filename(ns.infile)
 
+animation = importer.process(ns.infile)
 
-validator = TgsValidator(Severity.Error)
-validator(animation)
-if validator.errors:
-    sys.stderr.write("Could not export TGS:\n")
-    sys.stderr.write("\n".join(map(str, validator.errors))+"\n")
-    sys.exit(1)
+outfile = ns.outfile or ns.infile+".tgs"
 
+exporter = tgs.exporters.exporters.get_from_filename(outfile)
 
-with open(ns.outfile or ns.infile+".tgs", "wb") as tgs_file:
-    tgs.exporters.core.export_tgs(animation, tgs_file)
+if exporter.slug == "tgs":
+    validator = TgsValidator(Severity.Error)
+    validator(animation)
+    if validator.errors:
+        sys.stderr.write("Could not export TGS:\n")
+        sys.stderr.write("\n".join(map(str, validator.errors))+"\n")
+        sys.exit(1)
+
+exporter.process(animation, outfile)
