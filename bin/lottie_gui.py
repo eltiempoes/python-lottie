078af85a9887fef4bbd94706130b86205dc01bde
Mattia Basaglia <glax@dragon.best>
2020-04-25 20:31:58 +0200

    Add exporters to lottie_gui

--- bin/lottie_gui.py
+++ bin/lottie_gui.py
@@ -4,6 +4,7 @@ import os
 import sys
 import signal
 import argparse
+import threading
 from io import StringIO
 
 from PySide2 import QtCore, QtWidgets, QtGui, QtSvg
@@ -14,14 +15,171 @@ sys.path.insert(0, os.path.join(
 ))
 from lottie.importers.core import import_tgs
 from lottie.exporters.svg import export_svg
+from lottie.exporters.base import exporters
+from lottie.parsers.baseporter import IoProgressReporter, ExtraOption
 from lottie import __version__
+
+
+class GuiProgressReporter(IoProgressReporter):
+    def __init__(self):
+        self.dialogs = {}
+        self.threads = {}
+        self.lock = threading.Lock()
+        self.id = 0
+
+    def gen_id(self, parent):
+        self.id += 1
+        with self.lock:
+            dialog = QtWidgets.QProgressDialog(parent)
+            dialog.setCancelButton(None)
+            self.dialogs[self.id] = dialog
+        return self.id
+
+    def get_thread_dialog(self):
+        with self.lock:
+            id = threading.current_thread().ident
+            return self.dialogs[self.threads[id]]
+
+    def setup(self, title, id):
+        with self.lock:
+            tid = threading.current_thread().ident
+            self.threads[tid] = id
+            dialog = self.dialogs[id]
+
+        dialog.setWindowTitle(title)
+        dialog.hide()
+
+    def report_progress(self, title, value, total):
+        dialog = self.get_thread_dialog()
+        dialog.show()
+        dialog.setValue(value)
+        dialog.setMaximum(total)
+        dialog.setLabelText(title)
+
+    def report_message(self, message):
+        dialog = self.get_thread_dialog()
+        dialog.show()
+        dialog.setRange(0, 0)
+        dialog.setLabelText(message)
+
+    def cleanup(self):
+        with self.lock:
+            id = threading.current_thread().ident
+            self.dialogs.pop(self.threads.pop(id)).hide()
+
+
+class GuiExporter:
+    def __init__(self, exporter):
+        self.exporter = exporter
+        self.file_filter = "%s (%s)" % (exporter.name, " ".join("*.%s" % e for e in exporter.extensions))
+        self._dialog = None
+        self.widgets = {}
+
+    def _build_dialog(self):
+        self._dialog = QtWidgets.QDialog()
+        self._dialog.setWindowTitle("Export to %s" % self.exporter.name)
+        layout = QtWidgets.QFormLayout()
+        self._dialog.setLayout(layout)
+
+        for go in self.exporter.generic_options:
+            if go == "pretty":
+                self._add_option(layout, ExtraOption("pretty", action="store_true", help="Pretty print"))
+            if go == "frame":
+                self._add_option(layout, ExtraOption("frame", type=int, help="Frame to extract"))
+
+        for option in self.exporter.extra_options:
+            self._add_option(layout, option)
+
+        button_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self._dialog.accept)
+        button_box.rejected.connect(self._dialog.reject)
+        layout.addRow(button_box)
+
+    def _add_option(self, layout, option: ExtraOption):
+        if option.dest in self.widgets:
+            return
+        label = QtWidgets.QLabel(option.name.replace("_", " ").title())
+
+        default = option.kwargs.get("default", None)
+        option_type = option.kwargs.get("type", str)
+
+        if option.kwargs.get("action") == "store_true":
+            widget = QtWidgets.QCheckBox()
+            widget.setChecked(False)
+            getter = widget.isChecked
+        elif option.kwargs.get("action") == "store_false":
+            widget = QtWidgets.QCheckBox()
+            widget.setChecked(True)
+        elif "choices" in option.kwargs:
+            widget = QtWidgets.QComboBox()
+            for choice in option.kwargs["choices"]:
+                widget.addItem(str(choice))
+            if default:
+                widget.setCurrentText(str(default))
+            getter = lambda: option_type(widget.currentText())
+        elif option_type is int:
+            widget = QtWidgets.QSpinBox()
+            widget.setMinimum(-1000)
+            widget.setMaximum(1000)
+            if default is not None:
+                widget.setValue(int(default))
+            getter = widget.value
+        else:
+            widget = QtWidgets.QLineEdit()
+            if default is not None:
+                widget.setText(str(default))
+            getter = widget.text
+
+        help = option.kwargs.get("help", "")
+        widget.setWhatsThis(help)
+        widget.setToolTip(help)
+
+        widget.setObjectName(option.dest)
+        self.widgets[option.dest] = getter
+        layout.addRow(label, widget)
+
+    @property
+    def needs_dialog(self):
+        return self.exporter.generic_options or self.exporter.extra_options
+
+    @property
+    def dialog(self):
+        if not self._dialog:
+            self._build_dialog()
+        return self._dialog
+
+    def get_options(self):
+        return {
+            name: getter()
+            for name, getter in self.widgets.items()
+        }
+
+
+class ExportThread(QtCore.QThread):
+    def __init__(self, id, exporter, animation, file_name, options):
+        super().__init__()
+        self.id = id
+        self.animation = animation
+        self.exporter = exporter
+        self.file_name = file_name
+        self.options = options
+
+    def run(self):
+        IoProgressReporter.instance.setup("Export to %s" % self.exporter.name, self.id)
+        self.exporter.process(self.animation, self.file_name, **self.options)
+        IoProgressReporter.instance.cleanup()
+
+
+gui_eporters = list(map(GuiExporter, exporters))
 
 
 class LottieViewerWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.animation = None
+        self.dirname = ""
         self._frame_cache = {}
+        self._fu_gc = []
         self.setWindowTitle("Lottie Viewer")
 
         central_widget = QtWidgets.QWidget()
@@ -59,7 +217,8 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
 
         menu = self.menuBar()
         file_menu = menu.addMenu("&File")
-        self._make_action(file_menu, "&Open", "document-open", QtGui.QKeySequence.Open, self.dialog_open_file)
+        self._make_action(file_menu, "&Open...", "document-open", QtGui.QKeySequence.Open, self.dialog_open_file)
+        self._make_action(file_menu, "Save &As..", "document-save-as", QtGui.QKeySequence.SaveAs, self.dialog_save_as)
         self._make_action(file_menu, "&Quit", "application-exit", QtGui.QKeySequence.Quit, self.close)
 
         self.label_cahed = QtWidgets.QLabel()
@@ -76,9 +235,41 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
         menu.addAction(action)
 
     def dialog_open_file(self):
-        file_name, filter = QtWidgets.QFileDialog.getOpenFileName(self, "Open Animation", "", "Lottie (*.json *.tgs)")
+        file_name, filter = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open Animation", self.dirname, "Lottie (*.json *.tgs)"
+        )
         if file_name:
             self.open_file(file_name)
+
+    def dialog_save_as(self):
+        filters = ";;".join(ge.file_filter for ge in gui_eporters)
+        file_name, filter = QtWidgets.QFileDialog.getSaveFileName(self, "Save Animation", self.dirname, filters)
+
+        if not file_name:
+            return
+
+        for exporter in gui_eporters:
+            if exporter.file_filter == filter:
+                break
+        else:
+            return
+
+        if exporter.needs_dialog:
+            exporter.dialog.setParent(self)
+            exporter.dialog.setWindowFlags(QtGui.Qt.Dialog)
+            if exporter.dialog.exec_() != QtWidgets.QDialog.DialogCode.Accepted:
+                return
+
+        options = exporter.get_options()
+        if not os.path.splitext(file_name)[1]:
+            file_name += "." + exporter.exporter.extensions[0]
+
+        thread = ExportThread(
+            IoProgressReporter.instance.gen_id(self), exporter.exporter,
+            self.animation, file_name, options
+        )
+        thread.start()
+        self._fu_gc.append(thread)
 
     def open_file(self, file_name):
         self.stop()
@@ -94,6 +285,7 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
         self.display.setFixedSize(self.animation.width, self.animation.height)
         self._update_frame()
         self.setWindowTitle("Lottie Viewer - %s" % os.path.basename(file_name))
+        self.dirname = os.path.dirname(file_name)
 
     def _update_frame(self):
         if not self.animation:
@@ -159,6 +351,8 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     app = QtWidgets.QApplication([])
+
+    IoProgressReporter.instance = GuiProgressReporter()
 
     window = LottieViewerWindow()
     window.resize(800, 600)
