2b9c4eb387aa1d3959e71452cdf233d352bdcb7f
Mattia Basaglia <glax@dragon.best>
2020-04-25 18:14:55 +0200

    Allow playback on lottie_viewer

--- bin/lottie_viewer.py
+++ bin/lottie_viewer.py
@@ -44,7 +44,10 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
         self.display.setPalette(palette)
 
         self.layout_slider = QtWidgets.QHBoxLayout()
-        self.layout_slider.addWidget(QtWidgets.QLabel("frame"))
+        self.button_play = QtWidgets.QPushButton()
+        self.button_play.setCheckable(True)
+        self.button_play.toggled.connect(self.play_toggle)
+        self.layout_slider.addWidget(self.button_play)
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.layout_slider.addWidget(self.slider)
         self.slider_spin = QtWidgets.QSpinBox()
@@ -59,6 +62,13 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
         self._make_action(file_menu, "&Open", "document-open", QtGui.QKeySequence.Open, self.dialog_open_file)
         self._make_action(file_menu, "&Quit", "application-exit", QtGui.QKeySequence.Quit, self.close)
 
+        self.label_cahed = QtWidgets.QLabel()
+        self.statusBar().addPermanentWidget(self.label_cahed)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self._next_frame)
+        self.stop()
+
     def _make_action(self, menu, name, theme, key_sequence, trigger):
         action = QtWidgets.QAction(QtGui.QIcon.fromTheme(theme), name, self)
         action.setShortcut(key_sequence)
@@ -71,6 +81,7 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
             self.open_file(file_name)
 
     def open_file(self, file_name):
+        self.stop()
         self.animation = None
         self._frame_cache = {}
         animation = import_tgs(file_name)
@@ -88,16 +99,49 @@ class LottieViewerWindow(QtWidgets.QMainWindow):
         if not self.animation:
             return
 
-        frame = self.slider_spin.value()
-        if frame in self._frame_cache:
-            rendered = self._frame_cache[frame]
-        else:
-            file = StringIO()
-            export_svg(self.animation, file, frame)
-            rendered = file.getvalue().encode("utf-8")
-            self._frame_cache[frame] = rendered
-
+        rendered = self._render_frame(self.slider_spin.value())
         self.display.load(rendered)
+
+    def _render_frame(self, frame):
+        if frame in self._frame_cache:
+            return self._frame_cache[frame]
+
+        file = StringIO()
+        export_svg(self.animation, file, frame)
+        rendered = file.getvalue().encode("utf-8")
+        self._frame_cache[frame] = rendered
+        self.label_cahed.setText("%d%% Frames Rendered" % (
+            len(self._frame_cache) / (self.animation.out_point - self.animation.in_point) * 100
+        ))
+        return rendered
+
+    def stop(self):
+        self.timer.stop()
+        self.button_play.setIcon(QtGui.QIcon.fromTheme("media-playback-start"))
+        self.button_play.setText("Play")
+        self.button_play.setChecked(False)
+
+    def play(self):
+        if not self.animation:
+            self.stop()
+            return
+
+        self.button_play.setIcon(QtGui.QIcon.fromTheme("media-playback-stop"))
+        self.button_play.setText("Stop")
+
+        self.timer.start(1000/self.animation.frame_rate)
+
+    def _next_frame(self):
+        nf = self.slider.value() + 1
+        if nf > self.animation.out_point:
+            nf = self.animation.in_point
+        self.slider.setValue(nf)
+
+    def play_toggle(self, play):
+        if play:
+            self.play()
+        else:
+            self.stop()
 
 
 parser = argparse.ArgumentParser(description="GUI viewer for lottie Animations")
